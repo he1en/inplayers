d740da27104dbf73bcae5bfacbe1adf45367ae85
rachelgrau <rgrau@stanford.edu>
2015-05-06 23:25:32 -0700

    Get rid of duplicate load data

--- db/migrate/20150507044743_load_data.rb
+++
@@ -1,8 +0,0 @@
-class LoadData < ActiveRecord::Migration
-  def change
-    User.create(name: "Jose");
-    User.create(name: "Rachel G");
-    User.create(name: "Helen");
-    User.create(name: "Rachel L");
-  end
-end
